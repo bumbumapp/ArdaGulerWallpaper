4cfa1f63e6fac9e58dfad5ecfd9329fdce6fe86b
bumbumapp <bumbumapps@gmail.com>
2023-04-25 01:08:19 -0700

    changed

--- app/src/main/java/com/bumbumapps/wallpaper/utils/RawImageResult.kt
+++
@@ -1,6 +0,0 @@
-package com.bumbumapps.wallpaper.utils
-
-sealed class RawImageResult {
-    data class Success(val image: Int) : RawImageResult()
-    data class Error(val message: String) : RawImageResult()
-}
