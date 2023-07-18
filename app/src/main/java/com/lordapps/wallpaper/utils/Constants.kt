5ce8300d297819a4a6be02017e3597d4ed9fc7dd
bumbumapp <bumbumapps@gmail.com>
2023-07-18 07:59:41 -0700

    changed

--- app/src/main/java/com/lordapps/wallpaper/utils/Constants.kt
+++ app/src/main/java/com/lordapps/wallpaper/utils/Constants.kt
@@ -1,9 +1,9 @@
 package com.lordapps.wallpaper.utils
 
 object Constants {
-    const val LATEST_WALLPAPERS_STARTED_INDEX = 140
-    const val LATEST_WALLPAPERS_ENDED_INDEX = 170
-    const val PREMIUM_WALLPAPERS_STARTED_INDEX = 175
+    const val LATEST_WALLPAPERS_STARTED_INDEX = 10
+    const val LATEST_WALLPAPERS_ENDED_INDEX = 40
+    const val PREMIUM_WALLPAPERS_STARTED_INDEX = 205
     const val PREMIUM_WALLPAPERS_ENDED_INDEX = 214
 }
 
